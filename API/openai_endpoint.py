3460942722b3e7473b55dc86ca26f4e5ec345df3
Codegass <wchweichenhao@gmail.com>
2023-05-23 21:25:32 -0400

    openai api init

--- API/openai_endpoint.py
+++ API/openai_endpoint.py
@@ -1,16 +1,42 @@
 # The Endpoint to interact With OPENAI API
 import openai
-from SECRET_KEY import OPENAI_API_KEY
+import logging
 
-openai.api_key = OPENAI_API_KEY
 
-# Create a new stream
-stream = openai.Stream.create(
-  engine="text-davinci-003",
-  prompt="Once upon a time, in a kingdom far, far away,",
-  max_tokens=1000
-)
+class OpenAPI:
+    
+    def __init__(self, OPENAI_API_KEY):
+        openai.api_key = OPENAI_API_KEY
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
-# Fetch the stream output incrementally
-for output in stream:
-    print(output.choices[0].text.strip())
+    def get_chat_response(self, prompt):
+        openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = [
+                {"role": "system", "content": "You are a interviewee who are aksing coding questions related to Python"},
+                {"role": "user", "content": "Please answering the following interview questions detailedly, and if possible, plese provide the code with it."},
+                {"role": "asissstant", "content": "Sure, I will try my best to answer your coding interview questions."},
+            ]
+        )
+
+    def response_parser(self, response):
+        '''
+        Parse the chat response from the openai api 
+        and analyze the stop condition for further processing
+        '''
+
+        if response['choices'][0]['finish_reason'] == 'stop':
+            return response['choices'][0]['message']['content']
+
+        if response['choices'][0]['finish_reason'] == 'length':
+            self.logger.info("The response is too long, please consider to use the continue prompt")
+            return response['choices'][0]['message']['content']
+
+        if response['choices'][0]['finish_reason'] == 'content_filter':
+            self.logger.info("The content is blocked due to the content policy in the openai")
+            return response['choices'][0]['message']['content']
+      
+        if response['choices'][0]['finish_reason'] == 'null':
+            self.logger.info("API response still in progress or incomplete")
+            return response['choices'][0]['message']['content']
---
+++ main.py
@@ -0,0 +1,10 @@
+from SECRET_KEY import OPENAI_API_KEY
+from API.openai_endpoint import OpenAPI
+
+
+if __name__ == "__main__":
+    openai = OpenAPI(OPENAI_API_KEY)
+    prompt = "Explain the Transformer to me"
+    response = openai.get_response(prompt=prompt)
+    print(response)
+
